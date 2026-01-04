7e06b7d76a5875393998889b0962089afb42d262
francisco-josetti <franciscojosetti@gmail.com>
2026-01-04 19:16:05 -0300

    Adicionando a rota para os shows e linkando a mesma ao botão shows do index.html

--- app.py
+++ app.py
@@ -264,3 +264,7 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+@app.route('/shows_proximos')
+def shows_proximos():
+    return render_template('shows_proximos.html', user=current_user)
