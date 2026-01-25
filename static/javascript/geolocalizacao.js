e13a13175ca316e7023cab8c3036838247fe48ee
Enzo Wanderley Tomazoni <enzo.wanderley@academico.ifpb.edu.br>
2026-01-25 14:44:23 -0300

    excluindo o botao fantasma de configurações

--- static/javascript/geolocalizacao.js
+++ static/javascript/geolocalizacao.js
@@ -5,11 +5,12 @@ function erro(err) {
         `Erro ao obter localização: ${err.message}`;
 }
 
-
+//se conseguir a geolocalização
 function sucesso(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     const mapaLink = document.getElementById("map-link");
+    //gera um link para o streetview com a localização de quem está usando o site
     mapaLink.href = `https://www.google.com/maps?q=${latitude},${longitude}`;
     
     const formData = new URLSearchParams();
@@ -17,7 +18,7 @@ function sucesso(position) {
     formData.append("longitude", longitude);
      
 
-
+    //chama a função distancia de funcoes.py com o método post ( ATRAVÉS DE UM JSON )
     fetch("/distancia", {
         method: "POST",
         headers: {
@@ -33,7 +34,7 @@ function sucesso(position) {
         document.querySelectorAll(".distancia").forEach(td => {
             const titulo = td.dataset.titulo;
             const show = data.find(s => s.titulo === titulo);
-
+            //devolve mensagens de sucesso
             document.getElementById("status").textContent =
                 "Localização obtida com sucesso.";
             document.getElementById("status").classList.add("sucesso");
@@ -43,22 +44,25 @@ function sucesso(position) {
         });
     })
     .catch(err => {
+        //devolve mensagem de erro
         document.getElementById("status").textContent =
             `Erro ao obter endereço: ${err.message}`;
         document.getElementById("status").classList.add("erro");
     });
 }
 
+
+// Assim que a pagina carrega ela chama essa função
 window.addEventListener("load", () => {
     const statusParagrafo = document.getElementById("status");
     statusParagrafo.textContent = "Buscando localização...";
-
+    //confere se há suporte para geolocalização
     if (!navigator.geolocation) {
         statusParagrafo.textContent =
             "Seu navegador não suporta geolocalização.";
         return;
     }
-
+    //chama a função de geolocalização
     navigator.geolocation.getCurrentPosition(sucesso, erro);
 });
 
